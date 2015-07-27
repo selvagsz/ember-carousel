7325ecf5be9b6fdd286a5eb93241eefa62c96a61
selvaganesh.b <selvaganesh.b@zohocorp.com>
2015-07-27 23:09:12 +0530

    Unify carousel arrow components

---
+++ addon/components/carousel-arrow.js
@@ -0,0 +1,32 @@
+import Ember from 'ember';
+import layout from '../templates/components/carousel-arrow';
+
+const carouselArrowClassMap = {
+  left: 'carousel-left-arrow',
+  right: 'carousel-right-arrow'
+};
+
+const carouselSlideActionMap = {
+  left: 'slideLeft',
+  right: 'slideRight'
+};
+
+const computed = Ember.computed;
+
+export default Ember.Component.extend({
+  layout: layout,
+  carousel: Ember.inject.service(),
+  classNamesBindings: ['carousel-arrow-class'],
+  direction: 'left',
+
+  'carousel-arrow-class': computed('direction', {
+    get() {
+      return carouselArrowClassMap[this.get('direction')];
+    }
+  }),
+
+  click() {
+    var method = carouselSlideActionMap[this.get('direction')];
+    this.get('carousel')[method]();
+  }
+});
--- app/components/carousel-right-arrow.js
+++
@@ -1,1 +0,0 @@
-export { default } from 'ember-carousel/components/carousel-right-arrow';
